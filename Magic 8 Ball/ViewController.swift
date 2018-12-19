ca39a91223224813904b90ed98ef3023cd4da2f3
Dom <2montd10@solent.ac.uk>
2018-12-19 19:50:49 +0000

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Dom Montalto on 19/12/2018.
+//  Copyright © 2018 Dom Montalto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
